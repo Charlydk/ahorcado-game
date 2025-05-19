782e6bf16aee8b74f9b773c81e0e87cf98880728
Charlydk <89538903+Charlydk@users.noreply.github.com>
2025-05-18 21:54:06 -0300

    arreglando detalles del front

--- backend/AhorcadoBackend/AhorcadoBackend/obj/Debug/net8.0/AhorcadoBackend.AssemblyInfo.cs
+++ backend/AhorcadoBackend/AhorcadoBackend/obj/Debug/net8.0/AhorcadoBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AhorcadoBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a535a1d61d98b6597d63c03e475c08c4aa044b57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38994fde16061f2d6ad759e16d32162193292574")]
 [assembly: System.Reflection.AssemblyProductAttribute("AhorcadoBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AhorcadoBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
