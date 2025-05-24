431c75280bf3d184f5aaa9051fc3eba582151507
Charlydk <89538903+Charlydk@users.noreply.github.com>
2025-05-24 14:59:36 -0300

    no fuerzo el HTTPS con motivos de practica local

--- backend/AhorcadoBackend/AhorcadoBackend/Program.cs
+++ backend/AhorcadoBackend/AhorcadoBackend/Program.cs
@@ -46,7 +46,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection(); --> Descomentar si se quiere forzar HTTPS 
 
 // Habilitar el middleware de sesiones (debe ir antes de UseRouting/MapControllers)
 app.UseSession();
